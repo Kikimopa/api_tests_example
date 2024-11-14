ce1a823dc29469ac118fe84e1e80fe8c6ce63ebe
Леонид <Fear-1995@mail.ru>
2024-11-14 22:40:53 +0300

    add pre-commit

--- framework/Pet/endpoint/pet.py
+++ framework/Pet/endpoint/pet.py
@@ -2,9 +2,10 @@ from http import HTTPStatus
 
 from framework.base_api import BaseHandlers
 
+
 class PetHandlers(BaseHandlers):
 
-	def create(self, pet, **kwargs):
-		handler = "pet"
-		kwargs.setdefault("_ex_status_code", HTTPStatus.OK)
-		return self.client.post(handler=handler, json=pet, **kwargs)
+    def create(self, pet, **kwargs):
+        handler = "pet"
+        kwargs.setdefault("_ex_status_code", HTTPStatus.OK)
+        return self.client.post(handler=handler, json=pet, **kwargs)
--- framework/base_api.py
+++ framework/base_api.py
@@ -1,11 +1,10 @@
-from venv import logger
-
-import requests
 from json import JSONDecodeError
 from posixpath import join as urljoin
-from framework.logging import log_request, log_response
-import loguru
 
+import loguru
+import requests
+
+from framework.logging import log_request, log_response
 
 
 def return_json(func):
@@ -15,6 +14,7 @@ def return_json(func):
             return response.json()
         except JSONDecodeError:
             return response.text
+
     return wrapper
 
 
@@ -32,22 +32,22 @@ class BaseApi:
         self._auth = auth
 
     @return_json
-    def post(self, handler: str, json=None, data=None, files=None, headers:dict=None, **kwargs):
+    def post(self, handler: str, json=None, data=None, files=None, headers: dict = None, **kwargs):
         request_params = self._make_request_params(headers, handler)
         return self._request(method="POST", json=json, data=data, files=files, **kwargs, **request_params)
 
     @return_json
-    def get(self, handler: str, params:dict=None, headers: dict = None, **kwargs):
+    def get(self, handler: str, params: dict = None, headers: dict = None, **kwargs):
         request_params = self._make_request_params(headers, handler)
         return self._request(method="GET", params=params, **kwargs, **request_params)
 
     @return_json
-    def put(self, handler: str, json=None, data=None,  headers: dict = None, **kwargs):
+    def put(self, handler: str, json=None, data=None, headers: dict = None, **kwargs):
         request_params = self._make_request_params(headers, handler)
         return self._request(method="PUT", json=json, data=data, **kwargs, **request_params)
 
     @return_json
-    def patch(self, handler: str, json=None, data= None,  headers: dict = None, **kwargs):
+    def patch(self, handler: str, json=None, data=None, headers: dict = None, **kwargs):
         request_params = self._make_request_params(headers, handler)
         return self._request(method="PATCH", json=json, data=data, **kwargs, **request_params)
 
@@ -64,18 +64,20 @@ class BaseApi:
 
         return {"headers": resul_header, "url": result_url}
 
-
-    def _request(self, method: str, url: str, json=None, files=None, data=None, params:dict=None, headers:dict=None, **_):
-        request = requests.Request(method=method, url=url, json=json, files=files, data=data,params=params, headers=headers, auth=self._auth).prepare()
+    def _request(
+        self, method: str, url: str, json=None, files=None, data=None, params: dict = None, headers: dict = None, **_
+    ):
+        request = requests.Request(
+            method=method, url=url, json=json, files=files, data=data, params=params, headers=headers, auth=self._auth
+        ).prepare()
         log_request(logger=loguru.logger, request=request)
         with requests.Session() as session:
-            response = session.send(
-                request=request, verify=False
-            )
+            response = session.send(request=request, verify=False)
 
         log_response(logger=loguru.logger, response=response)
         return response
 
+
 class BaseHandlers:
-    def __init__(self, client:BaseApi):
+    def __init__(self, client: BaseApi):
         self.client = client
